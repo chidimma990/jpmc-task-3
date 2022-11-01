5900c7d5b22c495d8d3accc0595a661efcd37707
Nwankwo Abigail Chidimma <chidimmadonatus89@gmail.com>
2022-11-01 23:45:30 +0100

    Update Graph.tsx

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 import { Table } from '@finos/perspective';
 import { ServerRespond } from './DataStreamer';
-import { DataManipulator } from './DataManipulator';
+export interface Row {
+  price_abc: number,
+  price_def: number,
+  ratio: number,
+  timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
+}
 import './Graph.css';
 
-interface IProps {
-  data: ServerRespond[],
+export class DataManipulator{
+  static generateRow(ServerRespond: ServerRespond[]): Row {
+    const priceABC =(ServerRespond[0]. top_ask.price + ServerRespond[0].top_bid.price) / 2;
+    const priceDEF =(ServerRespond[1]. top_ask.price + ServerRespond[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1 + 0.5;
+    const lowerBound = 1 + 0.5;
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio,
+      timestamp: ServerRespond[0] . timestamp > ServerRespond[1].timestamp ?
+      ServerRespond[0].timestamp. ServerRespond[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: (ratio> upperBound || ratio<lowerBound) ? ratio : undefined,
+    };
+  }
 }
-
 interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
 }
@@ -22,11 +45,14 @@ class Graph extends Component<IProps, {}> {
     // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
-    const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
-      timestamp: 'date',
+    const schema ={
+      price_abc:'float',
+      price_def:'float',
+      ratio:"float",
+      timestamp:'date',
+      upper_bound:'float',
+      lower_bound:'float',
+      trigger_alert:'float',
     };
 
     if (window.perspective && window.perspective.worker()) {
@@ -38,21 +64,24 @@ class Graph extends Component<IProps, {}> {
       elem.setAttribute('view', 'y_line');
       elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio" , "lower_bound" , "upper_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
-        timestamp: 'distinct count',
-      }));
+        price_abc:'avg',
+        price_def:'avg',
+        ratio:'avg',
+        timestamp:'distinct count',
+        upper_bound:'avg',
+        lower_bound:'avg',
+        trigger_alert:'avg',
+      })) ;
     }
   }
 
   componentDidUpdate() {
     if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+      this.table.update([
+        DataManipulator.generateRow(this.prop.data),
+      ]as unknown as TableData);
     }
   }
 }
